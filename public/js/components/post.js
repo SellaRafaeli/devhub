b2efac90e2971d5256fd61eee17b19bfef9479f0
Sella Rafaeli <sella.rafaeli@gmail.com>
2016-10-30 12:06:35 +0200

    homepage vue post component

---
+++ public/js/components/post.js
@@ -0,0 +1,34 @@
+Vue.component('post', {
+  template: "#post-template",
+  props: ['post'],
+  data: function () {
+    return {
+      upvoted: false,
+      downvoted: false
+    };
+  },
+  methods: {
+    upvote: function () {
+      this.upvoted = !this.upvoted;
+      this.downvoted = false;
+    },
+    downvote: function () {
+      this.downvoted = !this.downvoted;
+      this.upvoted = false;
+    }
+  },
+  computed: {
+    link: function() {
+      return '/p/'+this.post.slug;
+    },
+    votes: function () {
+      if (this.upvoted) {
+        return this.post.votes + 1;
+      } else if (this.downvoted) {
+        return this.post.votes - 1;
+      } else {
+        return this.post.votes;
+      }
+    }
+  }
+});
