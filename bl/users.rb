a55a5578d1ca36baa9ffbf0ae72b0833856be65b
Lily Mateveyeva <lily.matveyeva@gmail.com>
2016-11-07 22:22:40 +0200

    username, setup for userpage

--- bl/users.rb
+++ bl/users.rb
@@ -1,5 +1,11 @@
 $users = $mongo.collection('users')
 
+get '/u/:username' do
+  post = $posts.get(username: params[:username])
+  to_page :'users/user_page', locals: {cu: cu}
+end
+
+
 get '/register' do 
   to_page(:'users/register')
 end
@@ -19,7 +25,7 @@ get '/me' do
 end
 
 post '/update_user' do
-	user_params = params.just(:email, :first_name, :last_name, :desc)
+	user_params = params.just(:email, :first_name, :last_name, :desc, :username)
 	user = $users.update_id(cuid, user_params)
 	{user: user}
 end
@@ -27,6 +33,7 @@ end
 post '/register' do 
   return false #should be just for admin 
   user_params = params.just(:email, :first_name, :last_name, :desc)
+  user_params[:username] =  get_unique_slug($users, :username, params[:email].split("@").first)
   user = $users.add(user_params)
   session[:user_id] = user['_id']
   {user: user}
